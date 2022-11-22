74102b2134c8e2b833d7694ae6444d2d9e78d030
zainidineafi <firmanmangkuraja@gmail.com>
2022-11-22 08:56:34 +0700

    Login akses dan dashboard template

---
+++ template/footer.inc.php
@@ -0,0 +1,13 @@
+<!-- Javascripts -->
+<script src="assets/plugins/jquery/jquery-3.1.0.min.js"></script>
+<script src="assets/plugins/bootstrap/popper.min.js"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="assets/plugins/chartjs/chart.min.js"></script>
+<script src="assets/plugins/apexcharts/dist/apexcharts.min.js"></script>
+<script src="assets/plugins/toastr/toastr.min.js"></script>
+<script src="assets/js/lime.min.js"></script>
+<script src="assets/js/pages/dashboard.js"></script>
+</body>
+
+</html>
