ec31db34562706380b8d08efb035d072caac64e9
Alif Triadi Agung W <aliftriadi.gaming@gmail.com>
2022-11-23 20:20:30 +0700

    Change

---
+++ assets/landing/js/app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  window.onscroll = function () {
+    console.log("scrolling");
+    let navbar = document.querySelector("nav.navbar");
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      navbar.classList.add("bg-white", "fixed-top");
+    } else {
+      navbar.classList.remove("bg-white", "fixed-top");
+    }
+  };
+});
--- index.php
+++ index.php
@@ -11,17 +11,24 @@ $faqs = Faq::get();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>PEDO Landing</title>
+    <title>Pet Adoption</title>
     <link rel="stylesheet" href="<?= base_url('assets/landing/bootstrap/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bebas+Neue&amp;display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?= base_url('assets/landing/css/styles.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/landing/css/app.css') ?>">
 </head>
 
 <body>
     <nav class="navbar navbar-light navbar-expand-md py-3">
-        <div class="container"><a class="navbar-brand d-flex align-items-center" href="#"><img width="100" height="80" src="<?= base_url('assets/landing/img/logo.svg') ?>"></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-3"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="#">
+                <img width="100" height="80" src="<?= base_url('assets/landing/img/logo.svg') ?>">
+            </a>
+            <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-3">
+                <span class="visually-hidden">Toggle navigation</span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navcol-3">
                 <ul class="navbar-nav mx-auto">
                     <li class="nav-item"><a class="nav-link active" href="#hero">Home</a></li>
@@ -29,7 +36,8 @@ $faqs = Faq::get();
                     <li class="nav-item"><a class="nav-link" href="#service">Service</a></li>
                     <li class="nav-item"><a class="nav-link" href="#faq">FAQ</a></li>
                     <li class="nav-item"><a class="nav-link" href="#testimonials">Testimonial</a></li>
-                </ul><button class="btn btn-primary btn-dark" type="button">Download</button>
+                </ul>
+                <button class="btn btn-primary btn-dark" type="button">Download</button>
             </div>
         </div>
     </nav>
@@ -219,7 +227,7 @@ $faqs = Faq::get();
     </footer>
     <script src="<?= base_url('assets/landing/bootstrap/js/bootstrap.min.js') ?>"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="<?= base_url('assets/landing/js/script.min.js') ?>"></script>
+    <script src="<?= base_url('assets/landing/js/app.js') ?>"></script>
     <script type="text/javascript">
         AOS.init();
     </script>
