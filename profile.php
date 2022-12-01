bd7175de9632629734d12170cdd22690d2d34648
Alif Triadi Agung W <aliftriadi.gaming@gmail.com>
2022-12-01 22:56:28 +0700

    change image view to img-fluid

--- profile.php
+++ profile.php
@@ -5,13 +5,11 @@ if (!isset($_SESSION['auth'])) {
     header('Location:' . base_url('login.php'));
 }
 
-
-
-use Models\Admin;   
+use Models\Admin;
 use Helper\Flash;
 use Rakit\Validation\Validator;
 
-$admin = Admin::find($_SESSION['auth']);    
+$admin = Admin::find($_SESSION['auth']);
 
 if (isset($_POST['_method']) && $_POST['_method'] == 'PUT') {
     $validator = new Validator;
@@ -27,10 +25,8 @@ if (isset($_POST['_method']) && $_POST['_method'] == 'PUT') {
     Flash::setFlash('success', 'Berhasil mengubah profile');
     header('Location:' . base_url('profile.php'));
 }
-                                 
+
 ?>
-
-
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/template/header.inc.php') ?>
 <div class="lime-container">
@@ -41,27 +37,27 @@ if (isset($_POST['_method']) && $_POST['_method'] == 'PUT') {
                     <div class="card">
                         <div class="card-body">
                             <!-- profile -->
-                            <img src="<?= $admin->image ?>" alt="<?= $admin->image; ?>">
+                            <img src="<?= $admin->image ?>" alt="<?= $admin->image; ?>" class="img-fluid">
                         </div>
                     </div>
                 </div>
-                
+
                 <div class="col-md-8">
                     <div class="card">
                         <div class="card-body">
                             <!-- Input -->
                             <form method="post">
-                            <input type="hidden" name="_method" value="PUT">
+                                <input type="hidden" name="_method" value="PUT">
                                 <div class="form-group">
                                     <label for="name">Name</label>
-                                    <input type="text" class="form-control" name="name" id="name" value="<?= $admin->name?>" required>
+                                    <input type="text" class="form-control" name="name" id="name" value="<?= $admin->name ?>" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="email">Email</label>
-                                    <input type="text" class="form-control" id="judul" value=<?= $admin->email?> readonly>
+                                    <input type="text" class="form-control" id="judul" value=<?= $admin->email ?> readonly>
                                 </div>
                                 <button class="btn btn-primary">Simpan</button>
-                            </form> 
+                            </form>
                         </div>
                     </div>
                 </div>
