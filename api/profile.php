08bde2b61d5cdd92eb822d16f04ca4dff0212e5c
Alif Triadi Agung W <aliftriadi.gaming@gmail.com>
2022-12-10 17:45:42 +0700

    change profile method from put, patch to post

--- api/profile.php
+++ api/profile.php
@@ -2,6 +2,7 @@
 header('Content-Type: application/json');
 require_once('../vendor/autoload.php');
 
+use Helper\Storage;
 use Models\User;
 use Models\UserAccessToken;
 use Rakit\Validation\Validator;
@@ -22,29 +23,52 @@ switch ($_SERVER['REQUEST_METHOD']) {
         $user = User::find($isLogin->user_id);
 
         return success_response('Berhasil mengambil user', compact('token', 'user'), 200);
-    case 'PUT':
-    case 'PATCH':
-        parse_str(file_get_contents("php://input"), $_PUT);
+    case 'POST':
+        try {
+            $validator = new Validator;
+            $validation = $validator->validate($_POST + $_FILES, [
+                'name' => 'required',
+                'image' => 'nullable|uploaded_file:0,2M,png,jpg,jpeg',
+                'confirm_password' => 'required',
+            ]);
 
-        $validator = new Validator;
-        $validation = $validator->validate($_PUT, [
-            'name' => 'required',
-        ]);
+            if ($validation->fails()) {
+                $errors = $validation->errors();
 
-        if ($validation->fails()) {
-            $errors = $validation->errors();
+                return error_response('Error Validasi', $errors->firstOfAll(), 400);
+            }
 
-            return error_response('Error Validasi', $errors->firstOfAll(), 400);
+            $user = User::findOrFail($isLogin->user_id);
+            $name = htmlspecialchars($_POST['name']);
+            $confirm_password = $_POST['confirm_password'];
+
+            return response_json([
+                'name' => $name,
+                'image' => $_FILES['image'],
+                'confirm_password' => $confirm_password,
+            ]);
+
+            if (has_uploaded_file($_FILES['image'])) {
+                if ($user->getRawOriginal('image') != 'default.jpg') {
+                    Storage::delete('storage/images/user/avatar', $user->getRawOriginal('image'));
+                }
+
+                $file = Storage::upload($_FILES['image'], 'storage/images/user/avatar');
+            }
+
+            if (!password_verify($confirm_password, $user->password)) {
+                return error_response('Password salah', null, 401);
+            }
+
+            $user->update([
+                'name' => $name,
+                'image' => $file ?? $user->getRawOriginal('image'),
+            ]);
+
+            return success_response('Berhasil mengubah user', $user);
+        } catch (\Exception $e) {
+            return error_response('Method Not Allowed, ' . $e->getMessage() . ' Code : ' . $e->getCode());
         }
-
-        $user = User::find($isLogin->user_id);
-        $name = $_PUT['name'];
-
-        $user->update([
-            'name' => $name,
-        ]);
-
-        return success_response('Berhasil mengubah user', $user);
     default:
         return error_response('Method Not Allowed');
 }
